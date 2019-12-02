ab3e2ba9b941842164484ac5f4648ee49428684e
Hervnzig <hervnzig1@gmail.com>
2019-12-02 18:28:11 +0200

    my first commit

---
+++ app/Http/Controllers/Home2Controller.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class Home2Controller extends Controller
+{
+    //
+    public function index(Request $request){
+        // return $_GET['name'];
+        // dd (die & dump)
+
+        // dd($request->all());
+        // dd($request);
+        // return $request->pass;
+
+        // echo $request->pass;
+        // echo $request->email;
+
+        // echo $request->get('name', 'Not provided');
+
+        return '<form method="post"><input type="text" name = "name"> <input type="submit"></form>';
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,96 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+// Route::get('/home', function(){
+//     echo Config('services.braintree.secret');
+// });
+
+
+
+// Route::get('home', function () {
+    
+// });
+
+// Route::post('home', function () {
+    
+// });
+
+// Route::delete('home', function () {
+    
+// });
+
+// Route::put('home', function () {
+    
+// });
+
+// Route::view('home', function () {
+    
+// });
+
+// Route::resource('home', function () {
+    
+// });
+
+
+
+// Route::get('/user/{id}/post/{postId}', function($id, $postID){
+//     echo "<h1>Your post is with id ".$id ." with a postID of " .$postID ."</h1>";
+// }); 
+
+
+// Route::get('/user/{id?}', function($id = null){
+//     echo $id ."<h1>With no ID ";
+// }); 
+
+
+// Route::prefix('admin')->group(function(){
+//     Route::get('/', function () {
+//         echo "Admin panel";
+//     });
+//     Route::get('/user', function(){
+//     echo "Admin user";
+//     });
+//     Route::get('/profile', function(){
+//         echo "Admin profile";
+//     });
+//     Route::get('/settings', function(){
+//         echo "Admin settings";
+//     });
+// });
+
+
+
+// Route::get('/home/landindpage', function() {
+//     echo "Home Page";
+// })->name('home');
+
+// Route::get('/page', function() {
+//     return redirect ()->route('home');
+// });
+
+
+
+
+// Route::get('/home', 'Home2Controller@index');
+
+
+Route::get('/contact', 'Home2Controller@index');
+// Route::post();
